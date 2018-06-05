777565046868db45be037de1bc05d01eb6a3ebe5
Volcaner <496310028@qq.com>
2018-06-05 08:57:02 +0800

    【kaican】 1. bytedance

---
+++ bytedance/src/js/index.js
@@ -0,0 +1,12 @@
+(function() {
+    var Index = function() {
+        var self = this;
+
+        this.init = function() {
+            console.log("aaa");
+        };
+    };
+
+    var Index = new Index();
+    Index.init();
+})();
