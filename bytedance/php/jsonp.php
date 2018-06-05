5a6f422636a848c74a8a6785c7dea1baa7a4d44f
Volcaner <496310028@qq.com>
2018-06-05 09:01:56 +0800

    [kaican]
    bytedance

--- bytedance/php/jsonp.php
+++ bytedance/php/jsonp.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
 header('Content-type: application/json');
 $jsoncallback = htmlspecialchars($_REQUEST ['jsoncallback']);  // 获取回调函数名
 $json_data = '["customername1", "customername2"]';  // json数据
 echo $jsoncallback . "(" . $json_data . ")";
-?>
+?> -->
